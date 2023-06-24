e5f15e69bdcfa7afe48dc58dfd5a25f2a5aa3dbc
Vladislav Pavelkov <pavelkovvv@yandex.ru>
2023-06-25 01:51:34 +0300

    Fix urls.py

--- it_solution_running_line/it_solution_running_line/urls.py
+++ it_solution_running_line/it_solution_running_line/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('running_line.urls')),b
+    path('', include('running_line.urls')),
 ]
